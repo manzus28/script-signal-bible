a7ab774180233c47b129c89a615152ee1f8786f7
Dandicode06 <danielepapa2006@gmail.com>
2026-04-25 18:00:23 +0200

    aggiunto funzioni per ogni parametro

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import openpyxl as xl
 import cantools as can
 
+import test
 from test import get_name_from_CAN0
 
 SIGNAL_BIBLE_FILE = "Signal_Bible_DP17.xlsx"
@@ -27,19 +28,46 @@ def access_signal_name(work_sheet):
     return signal_names_array
 
 # inizio + lunghezza + scale + offset + min + max + unità + source
+def access_signal_scale(work_sheet):
+    signal_scale_array = []
+    for signal_cell_tu in signal_sheet.iter_cols(min_col=15, max_col=15, min_row=4, max_row=526):
+        for signal_scale in signal_cell_tu:
+            signal_scale_array.append(signal_scale.value)
+    return signal_scale_array
 
+def access_signal_offset(work_sheet):
+    signal_offset_array = []
+    for signal_cell_tu in signal_sheet.iter_cols(min_col=14, max_col=14, min_row=4, max_row=526):
+        for signal_offset in signal_cell_tu:
+            signal_offset_array.append(signal_offset.value)
+    return signal_offset_array
 
+def access_signal_min_max(work_sheet):
+    signal_min_max_array = []
+    for signal_cell_tu in signal_sheet.iter_cols(min_col=10, max_col=10, min_row=4, max_row=526):
+        for signal_min_max in signal_cell_tu:
+            signal_min_max_array.append(signal_min_max.value)
+    return signal_min_max_array
 
-
-
+def access_signal_unit(work_sheet):
+    signal_unit_array = []
+    for signal_cell_tu in signal_sheet.iter_cols(min_col=5, max_col=5, min_row=4, max_row=526):
+        for signal_unit in signal_cell_tu:
+            signal_unit_array.append(signal_unit.value)
+    return signal_unit_array
 
 if __name__ == '__main__':
     signal_sheet = open_signal_bible(SIGNAL_BIBLE_FILE,SIGNAL_SHEET )
     signal_bible_names = access_signal_name(signal_sheet)
+    signal_bible_scale = access_signal_scale(signal_sheet)
+    signal_bible_offset = access_signal_offset(signal_sheet)
+    signal_bible_min_max = access_signal_min_max(signal_sheet)
+    signal_bible_unit = access_signal_unit(signal_sheet)
+
+    for signal_scale in signal_bible_scale:
+        print(signal_scale)
     signal_name_CAN0 = get_name_from_CAN0()
-    for signal_in_CAN in signal_name_CAN0:
-        if signal_in_CAN not in signal_bible_names:
-            print("not found: " + signal_in_CAN)
+    
 
 
 
